5499177c85ba5d481922c7740ba17c4cdb230fae
Mikhail Tyapkov <redcrownmailbox@gmail.com>
2023-08-30 01:08:46 +0300

    init server with node_modules

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
-const jsonServer = require('json-server')
-const clone = require('clone')
-const data = require('./db.json')
+const fs = require('fs');
+const jsonServer = require('json-server');
+const path = require('path');
 
-const isProductionEnv = process.env.NODE_ENV === 'production';
-const server = jsonServer.create()
+const server = jsonServer.create();
 
-// For mocking the POST request, POST request won't make any changes to the DB in production environment
-const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
-    _isFake: isProductionEnv
-})
-const middlewares = jsonServer.defaults()
+const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
 
-server.use(middlewares)
+server.use(jsonServer.defaults({}));
+server.use(jsonServer.bodyParser);
 
+// Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
+server.use(async (req, res, next) => {
+    await new Promise((res) => {
+        setTimeout(res, 800);
+    });
+    next();
+});
+
+// Эндпоинт для логина
+server.post('/login', (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+        const { users = [] } = db;
+
+        const userFromBd = users.find(
+            (user) => user.username === username && user.password === password,
+        );
+
+        if (userFromBd) {
+            return res.json(userFromBd);
+        }
+
+        return res.status(403).json({ message: 'User not found' });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ message: e.message });
+    }
+});
+
+// проверяем, авторизован ли пользователь
+// eslint-disable-next-line
 server.use((req, res, next) => {
-    if (req.path !== '/')
-        router.db.setState(clone(data))
-    next()
-})
+    if (!req.headers.authorization) {
+        return res.status(403).json({ message: 'AUTH ERROR' });
+    }
 
-server.use(router)
-server.listen(process.env.PORT || 8000, () => {
-    console.log('JSON Server is running')
-})
+    next();
+});
+
+server.use(router);
+
+// запуск сервера
+server.listen(8000, () => {
+    console.log('server is running on 8000 port');
+});
 
 
 module.exports = server
