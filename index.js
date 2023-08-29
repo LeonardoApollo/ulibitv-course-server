e7e3b55ad75af98b580502f34cf5a5cc87f84ced
Mikhail Tyapkov <redcrownmailbox@gmail.com>
2023-08-30 01:06:34 +0300

    init server template

--- index.js
+++ index.js
@@ -1,51 +1,28 @@
-const fs = require('fs');
-const jsonServer = require('json-server');
-const path = require('path');
+const jsonServer = require('json-server')
+const clone = require('clone')
+const data = require('./db.json')
 
-const server = jsonServer.create();
+const isProductionEnv = process.env.NODE_ENV === 'production';
+const server = jsonServer.create()
 
-const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
+// For mocking the POST request, POST request won't make any changes to the DB in production environment
+const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
+    _isFake: isProductionEnv
+})
+const middlewares = jsonServer.defaults()
 
-server.use(jsonServer.defaults({}));
-server.use(jsonServer.bodyParser);
+server.use(middlewares)
 
-// Эндпоинт для логина
-server.post('/login', (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
-        const { users = [] } = db;
-
-        const userFromBd = users.find(
-            (user) => user.username === username && user.password === password,
-        );
-
-        if (userFromBd) {
-            return res.json(userFromBd);
-        }
-
-        return res.status(403).json({ message: 'User not found' });
-    } catch (e) {
-        console.log(e);
-        return res.status(500).json({ message: e.message });
-    }
-});
-
-// проверяем, авторизован ли пользователь
-// eslint-disable-next-line
 server.use((req, res, next) => {
-    if (!req.headers.authorization) {
-        return res.status(403).json({ message: 'AUTH ERROR' });
-    }
+    if (req.path !== '/')
+        router.db.setState(clone(data))
+    next()
+})
 
-    next();
-});
+server.use(router)
+server.listen(process.env.PORT || 8000, () => {
+    console.log('JSON Server is running')
+})
 
-server.use(router);
-
-// запуск сервера/
-server.listen(8000, () => {
-    console.log('server is running on 8000 port');
-});
 
 module.exports = server
